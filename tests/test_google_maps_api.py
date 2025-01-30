b7772472e2976aae27cdb9e1e1baaaa3c71bd4eb
SergeiIgonin <igonin.sergei3@gmail.com>
2025-01-30 17:04:09 +0300

    в utils.checking добавил универс. метод по проверке значения выбранного поля и метод по проверке присутствия нужного слова в значении

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -11,6 +11,7 @@ class TestCreatePlace:
         print("\nМетод POST")
         result_post = GoogleMapsApi.create_new_place()  # отправка метода POST для создания новой локации
         Checking.check_json_fields(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
+        Checking.check_json_value(result_post, 'status', 'OK')
         Checking.check_status_code(result_post, 200)
         check_post = result_post.json()
         place_id = check_post.get("place_id")  # получения place_id для метода GET
@@ -23,6 +24,7 @@ class TestCreatePlace:
         print("\nМетод PUT")
         result_put = GoogleMapsApi.put_new_place(place_id)  # изменение данных о созданной локации
         Checking.check_json_fields(result_put, ['msg'])
+        Checking.check_json_value(result_put, 'msg', 'Address successfully updated')
         Checking.check_status_code(result_put, 200)
 
         print("Метод GET PUT")
@@ -33,11 +35,11 @@ class TestCreatePlace:
         print("\nМетод DELETE")
         result_delete = GoogleMapsApi.delete_new_place(place_id)  # удаление данных о созданной локации
         Checking.check_json_fields(result_delete, ['status'])
+        Checking.check_json_value(result_delete, 'status', 'OK')
         Checking.check_status_code(result_delete, 200)
 
         print("Метод GET DELETE")
         result_get = GoogleMapsApi.get_new_place(place_id)  # отправка метода Get
         Checking.check_json_fields(result_get, ['msg'])
+        Checking.check_json_search_word_in_value(result_get, 'msg', 'failed' )
         Checking.check_status_code(result_get, 404)
-
-
--- utils/checking.py
+++ utils/checking.py
@@ -6,12 +6,29 @@ class Checking:
     def check_status_code(result, status_code):
         """Метод для проверки статус кода"""
         assert status_code == result.status_code, 'ОШИБКА, Статус-код не совпадает'
-        print(f"Статус код = {result.status_code} (корректный)")
+        print(f'Статус код = {result.status_code} (корректный)')
 
     @staticmethod
     def check_json_fields(result, expected_fields):
-        """Метод для проверки наличия полей в ответе запроса"""
+        """Метод для проверки наличия обязательных полей в ответе запроса"""
         fields = json.loads(result.text)
         assert list(fields) == expected_fields, 'ОШИБКА, Список полей не совпадает'
         print(f'Обязательные поля: {list(fields)}')
-        print("Все поля присутствуют")
+        print('Все обязательные поля присутствуют в response')
+
+    @staticmethod
+    def check_json_value(result, field_name, expected_value):
+        """Метод для проверки значений обязательных полей в ответе запроса"""
+        check = result.json()
+        check_info = check.get(field_name)
+        assert check_info == expected_value, 'ОШИБКА, Значение поля не совпадает'
+        print(f'Значение поля {field_name} = {check_info}')
+        print(f'Значение поля {field_name} верно!')
+
+    @staticmethod
+    def check_json_search_word_in_value(result, field_name, search_word):
+        """Метод для проверки значений обязательных полей в ответе запроса при помощи поиска по определенному слову"""
+        check = result.json()
+        check_info = check.get(field_name)
+        assert search_word in check_info
+        print(f'Слово {search_word} присутствует в значении поля')
